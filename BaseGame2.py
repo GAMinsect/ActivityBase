0fd5959b537b353acea9dcdb9caee4dacb4a44b4
Massimo <140495094+GAMinsect@users.noreply.github.com>
2025-04-29 20:28:56 +0200

    Add files via upload

---
+++ BaseGame2.py
@@ -0,0 +1,55 @@
+import pygame
+import gi
+gi.require_version("Gtk","3.0")
+from gi.repository import Gtk
+from Button import *
+
+class BaseGame:
+    def __init__(self):
+        self.clock=pygame.time.Clock()
+
+    def initialize(self):
+        self.running=True
+        self.screen=pygame.display.get_surface()
+        self.width=self.screen.get_width()
+        self.height=self.screen.get_height()
+        self.player=pygame.Rect(100,100,100,100)
+        self.colors=[(100,100,100),(10,10,200),(200,10,0),(10,100,200)]
+        self.list=ColorSelection(4,self.colors,self.screen,10,10,200,100)
+        self.sq=Button((50,100,200),self.screen,400,400,100,100)
+        self.check=Button((188, 223, 145),self.screen,self.width//2-100,self.height//7,200,100,"CONTROLLA",pygame.font.SysFont(None,30))
+        self.run()
+
+        
+    def run(self):
+        def change_color(color,shape):
+            shape.color=color
+    
+        
+        value=0
+        cache_color=self.sq.color
+
+        while self.running:
+            self.screen.fill((94,252,237))
+
+            #Process Gtk messages
+            while Gtk.events_pending():
+                Gtk.main_iteration()
+            if not self.running:
+                break
+                
+            
+            for event in pygame.event.get(): 
+                if event.type==pygame.QUIT:
+                    return
+                if event.type==pygame.MOUSEBUTTONUP:
+                    self.screen.fill((180,180,150))
+                    self.list.draw()
+                    self.check.draw()
+                    self.sq.draw()
+            res=self.list.is_pressed() #Were I store the value if any button is pressed
+            if res!=None: change_color(res,self.sq)
+
+            pygame.draw.rect(self.screen,(value,0,0),self.player)
+            pygame.display.update()
+
