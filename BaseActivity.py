86ed3257452766edd2f623518455c8f914892d20
Massimo <140495094+GAMinsect@users.noreply.github.com>
2025-04-29 19:06:31 +0200

    Add files via upload

---
+++ BaseActivity.py
@@ -0,0 +1,47 @@
+import sys
+import gi
+gi.require_version("Gtk","3.0")
+from gi.repository import Gtk
+import pygame
+
+from sugar3.activity.activity import Activity
+from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.activity.widgets import ActivityToolbarButton
+from sugar3.graphics.toolbutton import ToolButton
+from sugar3.activity.widgets import StopButton
+
+import sugargame.canvas
+import BaseGame
+
+class Base(Activity):
+    def __init__(self,handle):
+        Activity.__init__(self,handle)
+
+        self.paused=False
+
+        #Create the game instance
+        self.game=BaseGame.BaseGame()
+
+        #Build the activity toolbar
+        self.build_toolbar()
+
+        self._pygamecanvas=sugargame.canvas.PygameCanvas(self,main=self.game.run,modules=[pygame.display])
+
+        self.set_canvas(self._pygamecanvas)
+        self._pygamecanvas.grab_focus()
+
+    def build_toolbar(self):
+        #It just has the closing button
+        toolbar_box=ToolbarBox()
+        self.set_toolbar_box(toolbar_box)
+        toolbar_box.show()
+
+        #Make the stop button
+        stop_button=StopButton(self)
+        toolbar_box.toolbar.insert(stop_button,-1)
+        stop_button.show()
+        stop_button.connect("clicked",self.stop)
+    
+    def stop(self):
+        self.game.running=False
+
---
+++ BaseGame.py
@@ -0,0 +1,38 @@
+import pygame
+import gi
+gi.require_version("Gtk","3.0")
+from gi.repository import Gtk
+
+class BaseGame:
+    def __init__(self):
+        self.clock=pygame.time.Clock()
+
+    def run(self):
+        self.running=True
+        screen=pygame.display.get_surface()
+        width=screen.get_width()
+        height=screen.get_height()
+        value=0
+        player=pygame.Rect(100,100,100,100)
+
+        while self.running:
+            screen.fill((94,252,237))
+
+            #Process Gtk messages
+            while Gtk.events_pending():
+                Gtk.main_iteration()
+            if not self.running:
+                break
+                
+            for event in pygame.event_get():
+                if event.type==pygame.QUIT:
+                    return
+                if event.type==pygame.MOUSEBUTTONDOWN:
+                    mouse_pos=pygame.mouse.get_pos()
+                    mouse_pressed=pygame.get_pressed()[0]
+                    if player.collidepoint(mouse_pos) and mouse_pressed:
+                        value+=1
+            
+            pygame.draw.rect(screen,(value,0,0),player)
+            pygame.display.update()
+
